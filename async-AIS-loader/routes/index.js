a1a66d85b222863001e2e6ced995fd98c76bd508
deeperx <albertodeprezzo@gmail.com>
2018-10-05 15:32:31 +0200

    project main routes structure definition

--- async-AIS-loader/routes/index.js
+++ async-AIS-loader/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Demo' });
+  res.render('index', { title: 'HOME PAGE' });
 });
 
 module.exports = router;
--- async-AIS-loader/routes/map.js
+++ async-AIS-loader/routes/map.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.send('#TODO: map rendering');
 });
 
 module.exports = router;
